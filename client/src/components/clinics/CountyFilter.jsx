5d330ecb369f8d23520b0fca83865ba408db4467
Lilian Kirwa <cheronlilian21@gmail.com>
2025-10-21 21:35:46 +0300

    Add CountyFilter component

--- client/src/components/clinics/CountyFilter.jsx
+++ client/src/components/clinics/CountyFilter.jsx
@@ -8,14 +8,3 @@ export default function CountyFilter({ county, setCounty }){
     </select>
   )
 }
-
-src/components/clinics/DistanceFilter.jsx
-export default function DistanceFilter({ distance, setDistance }){
-  return (
-    <div className="flex items-center gap-2">
-      <label className="text-sm text-gray-600">Radius</label>
-      <input type="range" min="1" max="100" value={distance} onChange={e=>setDistance(parseInt(e.target.value,10))} className="w-36" />
-      <span className="text-sm">{distance} km</span>
-    </div>
-  )
-}
