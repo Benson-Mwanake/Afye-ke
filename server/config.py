55cd2a757e6001ae75a113171fb18ebe392f5fca
JESSE MWENDWA NDUNDA <jessendunda2@gmail.com>
2025-10-28 13:30:53 +0300

    correct postgresql url

--- server/config.py
+++ server/config.py
@@ -5,7 +5,9 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-please-change")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://jesse:jesse123@localhost/afya_ke")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL", "postgresql://jesse:qwerty@localhost:5432/afya_ke"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secret-change-me")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=7)
