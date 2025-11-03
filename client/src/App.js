67bfb13d2e7c44ee1aaba8f35717f8e38f08ba4f
Benson <bensonmwanake79@gmail.com>
2025-11-03 22:28:05 +0300

    added a fotter to all pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
 import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
+import Footer from "./hooks/layouts/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <AppRoutes />
+        < Footer />
       </AuthProvider>
     </BrowserRouter>
   );
